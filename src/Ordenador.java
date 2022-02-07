68c802b12e40b4c9783f9ea095c0f51e827a24de
Raúl <rgarridorasillo@gmail.com>
2022-02-07 10:02:41 +0100

    T10Act1

---
+++ src/Ordenador.java
@@ -0,0 +1,59 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ordenador {
+
+	static Scanner teclado = new Scanner(System.in);
+	static int intentosRestantes = 5;
+	static int intentosHechos = 0;
+	static int numero = generarNumero();
+
+	public static int generarNumero() {
+		// El ordenador debe generar un número entre 1 y 500
+		int numero = (int) (Math.random() * 499) + 1;
+		return numero;
+	}
+
+	public void empezarJuego() {
+		int numeroIntento;
+
+		System.out.println(numero);
+		
+		do {
+			try {
+				
+				System.out.println("Introduce un numero del 1 al 500 - INTENTOS: " + intentosRestantes);
+				numeroIntento = teclado.nextInt();
+				intentosHechos++;
+				
+				comprobarNumero(numero, numeroIntento);
+				
+			} catch (InputMismatchException e) {
+				System.out.println("Valor introducido no reconocido, has perdido 1 intento.");
+				intentosRestantes--;
+			}
+			
+		} while (intentosRestantes > 0);
+		
+		System.out.println("Te has quedado sin intentos :(");
+	}
+
+	public static void comprobarNumero(int numero, int numeroIntento) {
+
+		if (numero == numeroIntento) {
+			System.out.println("Felicidades, has acertado el numero!!\nHas hecho " + intentosHechos + " intentos.");
+			System.exit(0);
+			
+		} else if (numero > numeroIntento){
+			System.out.println("Incorrecto, el numero es mayor a " + numeroIntento + ", has perdido 1 intento.");
+			intentosRestantes--;
+			
+		} else if (numero < numeroIntento){
+			System.out.println("Incorrecto, el numero es menor a " + numeroIntento + ", has perdido 1 intento.");
+			intentosRestantes--;
+			
+		}
+
+	}
+
+}
